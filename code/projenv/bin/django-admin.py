b13b707aabf66f2824b19c0a706a34d2fe0c27a8
dxb875 <dxb875@student.bham.ac.uk>
2020-06-16 13:20:25 +0100

    Initial upload of code and test environment materials

---
+++ code/projenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/davidb/Documents/Yr 2 Summer Coursework/Code/SummerPythonProject/code/projenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
