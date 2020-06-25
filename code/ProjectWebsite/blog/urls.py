9ca3dc3c13774b409e5f1f96dde867a623cdd943
dxb875 <dxb875@student.bham.ac.uk>
2020-06-25 17:07:09 +0100

    Changed names of certain items, began dev for academic section and looks alright - need to change background

--- code/ProjectWebsite/blog/urls.py
+++ code/ProjectWebsite/blog/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-path('', views.post_list, name='post_list'),
+path('', views.index, name='index'),
+path('academic.html',views.academic, name='academic'),
+path('achievement.html',views.achievement, name='achievement'),
+path('personal.html',views.personal, name='personal'),
+path('work.html',views.work, name='work'),
 ]
--- code/ProjectWebsite/blog/views.py
+++ code/ProjectWebsite/blog/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
-def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+def index(request):
+	return render(request, 'blog/index.html', {})
+def academic(request):
+	return render(request, 'blog/academic.html',{})
+def achievement(request):
+	return render(request, 'blog/achievement.html',{})
+def work(request):
+	return render(request, 'blog/work.html',{})
+def personal(request):
+	return render(request, 'blog/personal.html',{})
