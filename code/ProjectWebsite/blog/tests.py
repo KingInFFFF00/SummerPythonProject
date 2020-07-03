8f2d0d0d0b7058a7ddd37b9a4f9e17d5300fcbdc
dxb875 <dxb875@student.bham.ac.uk>
2020-07-03 13:55:11 +0100

    Continued Testing (Closer to blog)

--- code/ProjectWebsite/blog/tests.py
+++ code/ProjectWebsite/blog/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
-
+from django.urls import resolve
+from blog.views import post_list
+class SmokeTest(TestCase):
+	def test_url_resolve_to_home(self):
+		found = resolve('/')
+		self.assertEqual(found.func, post_list)
 # Create your tests here.
