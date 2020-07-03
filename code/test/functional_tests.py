c8d723e7cdebdedafbe7c46f70611ea25a6809c2
dxb875 <dxb875@student.bham.ac.uk>
2020-07-03 13:42:48 +0100

    Testing continues

--- code/test/functional_tests.py
+++ code/test/functional_tests.py
@@ -1,6 +1,21 @@
 from selenium import webdriver
-
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-
-assert 'Django' in browser.title
+import unittest
+ #SO, we have a website and we want to test that we can do the following with it:
+ #Update via a form that we have, which allows us to get this done.
+ #Consider - static vs dynamic
+ # Personal Statement -
+ # Academic History - Add to it
+ # Work History - Dynamic
+ # Personal Achievements - Dynamic
+ #We add to one, and then check on the page
+class NewVisitorTest(unittest.TestCase):
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+	def tearDown(self):
+		self.browser.quit()
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		self.browser.get('http://localhost:8000')
+		self.assertIn('Blog', self.browser.title)
+		self.fail('FinishTest')
+if __name__=='__main__':
+	unittest.main(warnings='ignore')
